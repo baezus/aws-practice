5dae042e3b5cde62fb684c459ee1cb05aa150a38
Elias <elias@baez.us>
2020-12-21 20:39:22 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'aws-practice',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-188-82-218.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:baezus/aws-practice.git',
+      path: '/home/ubuntu/aws-practice',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
